bfa2f697ca86ad926e078a65a76e3c832d6cadb9
Yan Shtein <85824489+YanShtein@users.noreply.github.com>
2022-08-06 09:26:37 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App from './App';
+
+
+class App extends Component {
+  render() {
+    return (
+      <h1>hello</h1>
+    )
+  }
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
